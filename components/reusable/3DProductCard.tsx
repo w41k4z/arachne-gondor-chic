9fe2815e8be196d1e2ea8a1fcff5720b50cadb1a
YohanAmbinintsoa <yohanrabepro@gmail.com>
2025-06-05 23:55:50 +0300

    Yohan feat : homepage first-screening

---
+++ components/reusable/3DProductCard.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+
+export function ThreeDCard() {
+  return (
+    <CardContainer className="inter-var">
+      <CardBody className="bg-amber-100 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.2] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl text-center w-full font-bold text-neutral-600 dark:text-white"
+        >
+          Produit du jour
+        </CardItem>
+
+        <CardItem translateZ="100" className="w-full mt-4">
+          <img
+            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex flex-col gap-2">
+          <CardItem
+
+            as="a"
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xl font-normal dark:text-white"
+          >
+            Poudre magique
+          </CardItem>
+          <CardItem
+
+            as="a"
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            200.000 Gondoariar
+          </CardItem>
+          <CardItem
+
+            as="a"
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            8 en stock
+          </CardItem>
+        </div>
+
+
+      </CardBody>
+    </CardContainer>
+  );
+}
