f2e2490e07ca1d08ed72f7248b32a52704094e5a
Andry Jonathan Randrianaly <jonathanrandrianaly@gmail.com>
2025-06-22 23:18:26 +0300

    creation table client

---
+++ backend/database/migrations/20250622201100-create-client.js
@@ -0,0 +1,32 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('client', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      numero: {
+        type: Sequelize.STRING
+      },
+      pseudo: {
+        type: Sequelize.STRING
+      },
+      motDePasse: {
+        type: Sequelize.STRING
+      },
+      nom: {
+        type: Sequelize.STRING
+      },
+      prenom: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('client');
+  }
+};
